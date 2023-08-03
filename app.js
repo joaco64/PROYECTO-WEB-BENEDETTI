a8840d3cd7bbdb96e855838165b85fa2bf705c49
Joaco64 <joacogutierrez478@gmail.com>
2023-08-03 03:53:54 -0300

    archivos nuevos y modificados

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
+
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/login', loginRouter );
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
