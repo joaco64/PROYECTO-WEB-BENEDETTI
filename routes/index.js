e71315f1745b13f6776335453d645fdd79ac7bff
Joaco64 <joacogutierrez478@gmail.com>
2023-08-16 08:27:45 -0300

    archivos nuevos y modificados

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,34 @@ var express = require("express");
 var router = express.Router();
 var nodemailer = require("nodemailer");
 var novedadesModel = require('../models/novedadesModels');
+var cloudinary = require("cloudinary").v2;
+
 
 /* GET home page. */
 router.get("/",  async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades()
+  novedades = novedades.splice(0, 6);
+  novedades = novedades.map((novedad) => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: "fill",
+      });
+      return {
+        ...novedad,
+        imagen,
+      };
+    } else {
+      return {
+        ...novedad,
+        imagen: '/images/noimage.jpg',
+      };
+    }
+  });
 
 
-  res.render("index", { novedades });
+  res.render('index', { novedades });
 });
 
 router.post("/", async (req, res, next) => {
